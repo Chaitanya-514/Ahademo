5ad314382bde661027e4a6a79a5111ffdffc3038
Chaitu Vinnu <Chaitu Vinnu@CHAITUVINI>
2023-01-06 14:07:55 +0530

    Ahademo project commit

---
+++ Aha02/src/main/java/com/aha/methods/Ahademoproject.java
@@ -0,0 +1,16 @@
+package com.aha.methods;
+
+public class Ahademoproject {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("welecome to github");
+		
+		System.out.println("new project ahaa");
+		
+	}
+	
+	
+	
+
+}
